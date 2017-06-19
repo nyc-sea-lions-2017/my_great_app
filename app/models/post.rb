3a3c2155a22ac680eba578e6fc850c145f2c46d1
Daniel Velez <velez.a.daniel@gmail.com>
2017-06-19 16:37:47 -0400

    fixed login

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ require 'friendly_id'
 class Post < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
-  belongs_to :author
+  belongs_to :author, optional: :true
 
   scope :most_recent, -> { order(id: :desc) }
 
